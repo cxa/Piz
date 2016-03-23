23c07f4d3744810c60e545b4b43ccf90443351fa
CHEN Xian-an <xianan.chen@gmail.com>
2016-03-23 19:58:38 +0800

    Remove unnecessary dependencies

--- SimpleUnzipper/SimpleUnzipper.h
+++ SimpleUnzipper/SimpleUnzipper.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 lazyapps. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SimpleUnzipper.
 FOUNDATION_EXPORT double SimpleUnzipperVersionNumber;
